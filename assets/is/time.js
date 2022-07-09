bfb0a752dcf0e7adcd3d92463c6f7c9abdd0b108
unknown-man <108600649+unknown-man@users.noreply.github.com>
2022-07-09 09:30:28 +0600

    Add files via upload

---
+++ assets/is/time.js
@@ -0,0 +1,13 @@
+//CONSTANTS
+
+const time_holder = document.getElementById("time_holder");
+const time_input = document.getElementById("time");
+//SELECTORS
+
+//FUNCTIONS
+    const d = new Date();
+    var time = d.getTime(); 
+    time_input.value = time;
+
+//FUNCTION CALL
+

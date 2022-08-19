b98db3b94258d42b1b8f021aa613f9b1e3b7712f
Hind Sagar Biswas <hindsbhk@gmail.com>
2022-08-19 21:18:50 +0600

    Add files via upload

--- error/404.php
+++ error/404.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>[ERROR] 404 - Page not found</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<br>
+<center class="border border-danger rounded p-2 m-1" style="max-width:500px;">
+<h2 class="text-danger">404 ERROR</h2>
+<h5 class="text-secondary">Page Not Found!</h5>
+<br>
+<a class="btn btn-danger" href="http://shinimark.epizy.com/">GO TO HOME</a>
+</center>
+</body>
+</html>

0d7d097abf311979788c551b77821faf6108eb82
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-06-08 16:48:07 +0600

    added filter

--- base/views.py
+++ base/views.py
@@ -4,13 +4,46 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q, F
 
 from .models import Categories, Statuses, ReadStatuses, Websites, Bookmarks
 from .forms import BookmarkForm
 
+def filter_bookmarks(queryset, request):
+    filters = request.GET
+
+    if filters.get('search'):
+        search_term = filters['search']
+        queryset = queryset.filter(Q(name__icontains=search_term) | Q(alt_names__icontains=search_term))
+    
+    if filters.get('category') and filters['category'] != 'none':
+        queryset = queryset.filter(category_id=filters['category'])
+    
+    if filters.get('status') and filters['status'] != 'none':
+        queryset = queryset.filter(status_id=filters['status'])
+    
+    if filters.get('reading_status') and filters['reading_status'] != 'none':
+        reading_status = filters['reading_status']
+        if reading_status == 'catched_up':
+            queryset = queryset.filter(current=F('latest'))
+        elif reading_status == 'reading':
+            queryset = queryset.filter(current__lt=F('latest'), current__gt=0)
+        elif reading_status == 'not_started':
+            queryset = queryset.filter(current=0)
+    
+    if filters.get('sort') and filters['sort'] != 'none':
+        sort_field = filters['sort']
+        order = '-' if filters.get('order') == 'desc' else ''
+        sort_field = order + sort_field
+        queryset = queryset.order_by(sort_field)
+    
+    return queryset
+
 # Create your views here.
 def home(request):
-    bookmarks_obj = Bookmarks.objects.all()
+    authority = 1 if request.user.is_authenticated else 0
+
+    bookmarks_obj = filter_bookmarks(Bookmarks.objects.all().filter(category__restriction__lte=authority), request)
     pagination = Paginator(bookmarks_obj, 50)
     page = request.GET.get('page')
 
@@ -18,9 +51,13 @@ def home(request):
     bookmarks.paginator_range = range(1, bookmarks.paginator.num_pages + 1)
     bookmarks.total = bookmarks_obj.count()
 
+
     context = {
         'title': "Personal bookmark site",
         'bookmarks': bookmarks,
+        'categories': Categories.objects.all().filter(restriction__lte=authority),
+        'statuses': Statuses.objects.all(),
+        'read_statuses': ReadStatuses.objects.all(),
     }
     return render(request, 'base/bookmarks/index.html', context)
 
@@ -89,6 +126,9 @@ def websites(request):
     return render(request, 'base/websites/index.html', context)
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('root')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
